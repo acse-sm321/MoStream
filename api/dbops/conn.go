98639f068f0d9b1552efe0effbd42bb5b0098791
acse-sm321 <shuheng.mo21@imperial.ac.uk>
2022-02-16 13:23:31 +0000

    Added API test workflow

--- api/dbops/conn.go
+++ api/dbops/conn.go
@@ -11,8 +11,17 @@ var (
 )
 
 func init() {
-	dbConn, err = sql.Open("mysql", "root:19990429@tcp(localhost:3306)/mosteam?charset=utf8")
+	dbConn, err = sql.Open("mysql", "root:19990429@tcp(localhost:3306)/mostream?charset=utf8")
 	if err != nil {
 		panic(err.Error())
 	}
 }
+
+// create databases/
+
+// users
+//CREATE TABLE `users`(
+//`id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+//`login_name` VARCHAR(64) NOT NULL UNIQUE KEY,
+//`pwd` TEXT DEFAULT NULL
+//)ENGINE InnoDB
